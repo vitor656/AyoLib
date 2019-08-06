8913f73b6391cb5088c12856b990c9f057bfb5a9
vitor656 <vitor.rabelo.656@gmail.com>
2019-08-06 13:49:59 -0300

    Basic classes

---
+++ Component.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AyoLib
+{
+    public class Component
+    {
+        private Entity _entity;
+        
+        private void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+
+        protected virtual void Update()
+        {
+
+        }
+
+        protected virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ Entity.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AyoLib
+{
+    public class Entity
+    {
+        private List<Component> _components;
+
+        public bool Active = true;
+        public Vector2 Position = Vector2.Zero;
+
+        public string Tag { get; set; }
+
+        public float X
+        {
+            get { return Position.X; }
+            set { Position.X = value; }
+        }
+
+        public float Y
+        {
+            get { return Position.Y; }
+            set { Position.Y = value; }
+        }
+
+        public Entity()
+        {
+            Position = Vector2.Zero;
+        }
+
+        public Entity(Vector2 position)
+        {
+            Position = position;
+            _components = new List<Component>();
+        }
+
+        protected void AddComponent(Component component)
+        {
+            _components.Add(component);
+        }
+
+        protected Component GetComponent<T>() where T : Component
+        {
+            foreach (var component in _components)
+            {
+                if(component.GetType() == typeof(T))
+                {
+                    return component;
+                }
+            }
+
+            return null;
+        }
+
+        protected void RemoveComponent(Component component)
+        {
+            _components.Remove(component);
+        }
+        
+    }
+}

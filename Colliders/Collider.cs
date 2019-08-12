cb65953c661791f81868daf46a292e9889885b14
vitor656 <vitor.rabelo.656@gmail.com>
2019-08-12 08:59:39 -0300

    .

--- Colliders/Collider.cs
+++ Colliders/Collider.cs
@@ -9,9 +9,37 @@ namespace AyoLib.Colliders
     {
         public Rectangle Rectangle;
 
+        private Dictionary<AyoBasic, Action> _objectsToCollideWith;
+
+        public Collider()
+        {
+            _objectsToCollideWith = new Dictionary<AyoBasic, Action>();
+        }
+
         public override void Update(GameTime gameTime)
         {
+            CheckCollisions();
+
             base.Update(gameTime);
+        }
+
+        public void Intersects(AyoBasic other, Action Callback)
+        {
+            _objectsToCollideWith.Add(other, Callback);
+        }
+
+        private void CheckCollisions()
+        {
+            foreach (var obj in _objectsToCollideWith)
+            {
+                if(obj.Key.HitBox != null)
+                {
+                    if(Rectangle.Intersects(obj.Key.HitBox.Rectangle))
+                    {
+                        obj.Value();
+                    }
+                }
+            }
         }
     }
 }

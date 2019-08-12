e98f0eaeee6c0fb83da5e7f18f405508ae1c7a88
vitor656 <vitor.rabelo.656@gmail.com>
2019-08-12 13:41:59 -0300

    .

--- Colliders/Collider.cs
+++ Colliders/Collider.cs
@@ -7,18 +7,33 @@ namespace AyoLib.Colliders
 {
     public class Collider : Component
     {
-        public Rectangle Rectangle;
+        public Rectangle HitBox;
 
         private Dictionary<AyoBasic, Action> _objectsToCollideWith;
 
         public Collider()
         {
             _objectsToCollideWith = new Dictionary<AyoBasic, Action>();
+
+            if(Owner != null)
+            {
+                if(Owner.GetGraphic() != null && Owner.GetGraphic().Texture2D != null)
+                {
+                    HitBox = new Rectangle((int) Owner.Position.X, (int) Owner.Position.Y, Owner.GetGraphic().Texture2D.Width, Owner.GetGraphic().Texture2D.Height);
+                }
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
+            Console.WriteLine("Collider Update");
             CheckCollisions();
+
+            if(HitBox != null)
+            {
+                HitBox.X = (int) Owner.Position.X;
+                HitBox.Y = (int) Owner.Position.Y;
+            }
 
             base.Update(gameTime);
         }
@@ -34,7 +49,7 @@ namespace AyoLib.Colliders
             {
                 if(obj.Key.HitBox != null)
                 {
-                    if(Rectangle.Intersects(obj.Key.HitBox.Rectangle))
+                    if(HitBox.Intersects(obj.Key.HitBox.HitBox))
                     {
                         obj.Value();
                     }

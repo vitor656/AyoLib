ecb924991c4a85e0ab954d420461707e5e1af5eb
vitor656 <vitor.rabelo.656@gmail.com>
2019-09-19 08:31:14 -0300

    Background class

---
+++ Entities/Background.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AyoLib.Entities
+{
+    public class Background : Entity
+    {
+        public int ScrollX { get; set; }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            var source = new Rectangle(ScrollX, 0, Graphic.Width, Graphic.Height);
+            spriteBatch.Draw(Graphic.Texture2D, Position, source, Color.White);
+        }
+    }
+}
